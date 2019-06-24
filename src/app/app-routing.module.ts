02093736a4bbaac904c1c4b00fda840884124445
Glaucos Luiz Ginez <glaucosginez@gmail.com>
2019-06-24 18:32:12 -0300

    Teste

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  },
+  { path: 'list2', loadChildren: './list2/list2.module#List2PageModule' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/list2/list2.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list2',
+  templateUrl: './list2.page.html',
+  styleUrls: ['./list2.page.scss'],
+})
+export class List2Page implements OnInit {
+
+  teste = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
